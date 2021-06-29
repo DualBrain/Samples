72d6e83b53d25f802b59c4bbc0c0c0724dd0ee7f
Cory Smith <dualbrain@msn.com>
2021-06-29 08:39:37 -0500

    Upgrading CodeAnalysis.VB to 3.10.0.

--- TestNuGetPackages/Program.vb
+++ TestNuGetPackages/Program.vb
@@ -8,11 +8,17 @@ End Module
 
 Public Interface IPerson
   Property Name As String
+
+  Property Title As string
 End Interface
 
 Public Class Employee
   Implements IPerson
 
+  'Private _Title As string
+
   Public Property Name As String
+
+  Public property Title As String Implements IPerson.title
 
 End Class
