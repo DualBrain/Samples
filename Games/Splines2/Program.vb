373f901cfc3ad9ff4e4aaca4e49d7177b2edf991
Cory Smith <dualbrain@msn.com>
2023-03-11 22:26:17 -0600

    Completed Splines2.

--- Games/Splines2/Program.vb
+++ Games/Splines2/Program.vb
@@ -25,18 +25,16 @@ Class Splines2
   Private m_selectedPoint As Integer
   Private m_marker As Double
 
+  Private m_modelCar As List(Of (Single, Single))
+
   Public Overrides Function OnUserCreate() As Boolean
 
-    m_path.Points.Add(New Point2D(10, 41))
-    m_path.Points.Add(New Point2D(20, 41))
-    m_path.Points.Add(New Point2D(30, 41))
-    m_path.Points.Add(New Point2D(40, 41))
-    m_path.Points.Add(New Point2D(50, 41))
-    m_path.Points.Add(New Point2D(60, 41))
-    m_path.Points.Add(New Point2D(70, 41))
-    m_path.Points.Add(New Point2D(80, 41))
-    m_path.Points.Add(New Point2D(90, 41))
-    m_path.Points.Add(New Point2D(100, 41))
+    For i = 0 To 9
+      m_path.Points.Add(New Point2D(30 * Math.Sin(i / 10 * 3.14159 * 2) + ScreenWidth() / 2,
+                                       30 * Math.Cos(i / 10 * 3.14159 * 2) + ScreenHeight() / 2))
+    Next
+
+    m_modelCar = New List(Of (Single, Single)) From {(1, 1), (1, 3), (3, 0), (0, -3), (-3, 0), (-1, 3), (-1, 1)}
 
     Return True
 
@@ -64,11 +62,11 @@ Class Splines2
     If m_keys(VK_RIGHT).bHeld Then m_path.Points(m_selectedPoint).X += 30 * elapsedTime
     If m_keys(VK_UP).bHeld Then m_path.Points(m_selectedPoint).Y -= 30 * elapsedTime
     If m_keys(VK_DOWN).bHeld Then m_path.Points(m_selectedPoint).Y += 30 * elapsedTime
-    If m_keys(AscW("A")).bHeld Then m_marker -= 5.0 * elapsedTime
-    If m_keys(AscW("S")).bHeld Then m_marker += 5.0 * elapsedTime
+    If m_keys(AscW("A")).bHeld Then m_marker -= 20.0 * elapsedTime
+    If m_keys(AscW("S")).bHeld Then m_marker += 20.0 * elapsedTime
 
-    If m_marker > m_path.Points.Count - 1 Then m_marker -= m_path.Points.Count
-    If m_marker < 0 Then m_marker += m_path.Points.Count
+    If m_marker > m_path.TotalSplineLength Then m_marker -= m_path.TotalSplineLength
+    If m_marker < 0 Then m_marker += m_path.TotalSplineLength
 
     ' Draw Spline
     For t = 0.0 To m_path.Points.Count Step 0.005
@@ -76,10 +74,15 @@ Class Splines2
       Draw(pos.X, pos.Y)
     Next
 
+    m_path.TotalSplineLength = 0.0
+
     ' Draw Control Points
     For i = 0 To m_path.Points.Count - 1
+      m_path.Points(i).Length = m_path.CalculateSegmentLength(i, True)
+      m_path.TotalSplineLength += m_path.Points(i).Length
       Fill(m_path.Points(i).X - 1, m_path.Points(i).Y - 1, m_path.Points(i).X + 2, m_path.Points(i).Y + 2, PIXEL_SOLID, FG_RED)
       DrawString(m_path.Points(i).X, m_path.Points(i).Y, CStr(i))
+      DrawString(m_path.Points(i).X + 3, m_path.Points(i).Y, CStr(m_path.Points(i).Length))
     Next
 
     ' Highlight control point
@@ -87,10 +90,16 @@ Class Splines2
     DrawString(m_path.Points(m_selectedPoint).X, m_path.Points(m_selectedPoint).Y, CStr(m_selectedPoint))
 
     ' Draw agent to demonstrate gradient
-    Dim p1 = m_path.GetSplinePoint(m_marker, True)
-    Dim g1 = m_path.GetSplineGradient(m_marker, True)
+    Dim offset = m_path.GetNormalisedOffset(m_marker)
+    Dim p1 = m_path.GetSplinePoint(offset, True)
+    Dim g1 = m_path.GetSplineGradient(offset, True)
     Dim r = Math.Atan2(-g1.Y, g1.X)
     DrawLine(5.0F * Math.Sin(r) + p1.X, 5.0F * Math.Cos(r) + p1.Y, -5.0F * Math.Sin(r) + p1.X, -5.0F * Math.Cos(r) + p1.Y, PIXEL_SOLID, FG_BLUE)
+
+    DrawWireFrameModel(m_modelCar, CSng(p1.X), CSng(p1.Y), CSng(-r + (3.14159F / 2.0F)), 5.0F, FG_CYAN)
+
+    DrawString(2, 2, CStr(offset))
+    DrawString(2, 4, CStr(m_marker))
 
     Return True
 
@@ -101,6 +110,7 @@ End Class
 Class Point2D
   Public Property X As Double
   Public Property Y As Double
+  Public Property Length As Double
   Public Sub New()
   End Sub
   Public Sub New(x As Double, y As Double)
@@ -112,12 +122,13 @@ End Class
 Class Spline
 
   Public Property Points As New List(Of Point2D)
+  Public Property TotalSplineLength As Double
 
-  Public Function GetSplinePoint(t As Double, Optional bLooped As Boolean = False) As Point2D
+  Public Function GetSplinePoint(t As Double, Optional looped As Boolean = False) As Point2D
 
     Dim p0, p1, p2, p3 As Integer
 
-    If Not bLooped Then
+    If Not looped Then
       p1 = CInt(Fix(t)) + 1
       p2 = p1 + 1
       p3 = p2 + 1
@@ -145,11 +156,11 @@ Class Spline
 
   End Function
 
-  Public Function GetSplineGradient(t As Double, Optional bLooped As Boolean = False) As Point2D
+  Public Function GetSplineGradient(t As Double, Optional looped As Boolean = False) As Point2D
 
     Dim p0, p1, p2, p3 As Integer
 
-    If Not bLooped Then
+    If Not looped Then
       p1 = CInt(Fix(t)) + 1
       p2 = p1 + 1
       p3 = p2 + 1
@@ -176,6 +187,35 @@ Class Spline
 
     Return New Point2D(tx, ty)
 
+  End Function
+
+  Public Function CalculateSegmentLength(node As Integer, Optional looped As Boolean = False) As Double
+
+    Dim length = 0.0
+    Dim stepSize = 0.005
+    Dim oldPoint, newPoint As Point2D
+
+    oldPoint = GetSplinePoint(node, looped)
+
+    For t = 0 To 1 - stepSize Step stepSize
+      newPoint = GetSplinePoint(node + t, looped)
+      length += Math.Sqrt((newPoint.X - oldPoint.X) * (newPoint.X - oldPoint.X) + (newPoint.Y - oldPoint.Y) * (newPoint.Y - oldPoint.Y))
+      oldPoint = newPoint
+    Next
+
+    Return length
+
+  End Function
+
+  Public Function GetNormalisedOffset(p As Double) As Double
+    ' Which node is the base?
+    Dim i = 0
+    While p > Points(i).Length
+      p -= Points(i).Length
+      i += 1
+    End While
+    ' The fractional is the offset 
+    Return i + (p / Points(i).Length)
   End Function
 
 End Class
