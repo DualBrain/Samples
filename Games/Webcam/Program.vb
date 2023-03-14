35aeeceebd6e592775ad0734110b7ca9d25545c0
Cory Smith <dualbrain@msn.com>
2023-03-14 17:15:43 -0500

    Minor improvements to Webcam (console).

--- Games/Webcam/Program.vb
+++ Games/Webcam/Program.vb
@@ -11,7 +11,7 @@ Module Program
 
   Sub Main() 'args As String())
     Dim game As New Webcam
-    game.ConstructConsole(160, 80, 8, 8)
+    game.ConstructConsole(640, 480, 2, 2)
     game.Start()
   End Sub
 
@@ -53,13 +53,20 @@ Class Webcam
 
           ' Convert into char / color combo 
 
-          ' Draw Pixel
+          Dim sym = 0
+          Dim bg_col = 0
+          Dim fg_col = 0
 
-          If g > 0.5 Then
-            Draw(x, y, PIXEL_SOLID, FG_WHITE)
-          Else
-            Draw(x, y, PIXEL_SOLID, FG_BLACK)
-          End If
+          Classify_Grey(r, g, b, sym, bg_col, fg_col)
+
+          ' Draw Pixel
+          Draw(x, y, sym, bg_col Or fg_col)
+          'If g > 0.5 Then
+          '  Draw(x, y, PIXEL_SOLID, FG_WHITE)
+          'Else
+          '  Draw(x, y, PIXEL_SOLID, FG_BLACK)
+          'End If
+
         Next
       Next
     End If
@@ -67,6 +74,32 @@ Class Webcam
     Return True
 
   End Function
+
+  Private Sub Classify_Grey(r As Double, g As Double, b As Double, ByRef ch As Integer, ByRef fg As Integer, ByRef bg As Integer)
+
+    Dim luminance = 0.2987 * r + 0.587 * g + 0.114 * b
+    Dim pixel_bw = CInt(Fix(luminance * 13.0))
+    Select Case pixel_bw
+      Case 0 : bg = BG_BLACK : fg = FG_BLACK : ch = PIXEL_SOLID
+
+      Case 1 : bg = BG_BLACK : fg = FG_DARK_GREY : ch = PIXEL_QUARTER
+      Case 2 : bg = BG_BLACK : fg = FG_DARK_GREY : ch = PIXEL_HALF
+      Case 3 : bg = BG_BLACK : fg = FG_DARK_GREY : ch = PIXEL_THREEQUARTERS
+      Case 4 : bg = BG_BLACK : fg = FG_DARK_GREY : ch = PIXEL_SOLID
+
+      Case 5 : bg = BG_GREY : fg = FG_GREY : ch = PIXEL_QUARTER
+      Case 6 : bg = BG_GREY : fg = FG_GREY : ch = PIXEL_HALF
+      Case 7 : bg = BG_GREY : fg = FG_GREY : ch = PIXEL_THREEQUARTERS
+      Case 8 : bg = BG_GREY : fg = FG_GREY : ch = PIXEL_SOLID
+
+      Case 9 : bg = BG_GREY : fg = FG_WHITE : ch = PIXEL_QUARTER
+      Case 10 : bg = BG_GREY : fg = FG_WHITE : ch = PIXEL_HALF
+      Case 11 : bg = BG_GREY : fg = FG_WHITE : ch = PIXEL_THREEQUARTERS
+      Case 12 : bg = BG_GREY : fg = FG_WHITE : ch = PIXEL_SOLID
+
+    End Select
+
+  End Sub
 
   Private Shared Function MatToBitmap(mat As Mat) As Bitmap
     Using ms = mat.ToMemoryStream()
